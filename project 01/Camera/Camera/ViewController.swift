990ceac1ea010d6fe357f617fc21988b35a10fdf
Silvostok <testing repo>
2016-02-18 14:44:59 +0100

    new

---
+++ project 01/Camera/Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Miguel Angel Rubio Caballero on 18/02/16.
+//  Copyright © 2016 MiguelRubio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
