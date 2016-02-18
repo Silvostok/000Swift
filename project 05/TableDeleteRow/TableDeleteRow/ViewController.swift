e4907ab5a96b011a1a40b1d2f7c14d214a1f6149
Silvostok <testing repo>
2016-02-18 20:30:40 +0100

    delete info

---
+++ project 05/TableDeleteRow/TableDeleteRow/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TableDeleteRow
+//
+//  Created by Miguel Angel Rubio Caballero on 18/02/16.
+//  Copyright © 2016 MiguelRubio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  
+
+
+}
+
