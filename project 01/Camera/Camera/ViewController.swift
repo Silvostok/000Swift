dcbd6bb3f586da2ad0a36fb1296a60da42c8c646
Silvostok <testing repo>
2016-02-19 16:22:30 +0100

    image

--- project 01/Camera/Camera/ViewController.swift
+++ project 01/Camera/Camera/ViewController.swift
@@ -8,15 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var bottomMenu: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
   }
-
   
-
-
+  
+  // MARK: New Photo
+  @IBAction func newPhotoButtonPressed(sender: AnyObject) {
+    
+    
+    let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+    
+    actionSheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { action in
+      self.showCamera()
+    }))
+    
+    actionSheet.addAction(UIAlertAction(title: "Album", style: .Default, handler: { action in
+      self.showAlbum()
+      
+    }))
+    
+    actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+    self.presentViewController(actionSheet, animated: true, completion: nil)
+    
+  }
+  
+  func showCamera() {
+    let cameraPicker = UIImagePickerController()
+    cameraPicker.delegate = self
+    cameraPicker.sourceType = .Camera
+    self.presentViewController(cameraPicker, animated: true, completion: nil)
+  }
+  
+  func showAlbum() {
+    let cameraPicker = UIImagePickerController()
+    cameraPicker.delegate = self
+    cameraPicker.sourceType = .PhotoLibrary
+    self.presentViewController(cameraPicker, animated: true, completion: nil)
+  }
+  
+  // MARK: Share
+  @IBAction func shareButtonPressed(sender: AnyObject) {
+    let activityController = UIActivityViewController(activityItems: ["Check out our really cool app", imageView.image!], applicationActivities: nil)
+    
+    presentViewController(activityController, animated: true, completion: nil)
+  }
+  
+  
+  
+  
+  // MARK: UIImagePickerControllerDelegate
+  func imagePickerController(picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+      dismissViewControllerAnimated(true, completion: nil)
+      if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        imageView.image = image
+      }
+  }
+  
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  
+  
 }
 
--- project 05/TableDeleteRow/TableDeleteRow/ViewController.swift
+++ project 05/TableDeleteRow/TableDeleteRow/ViewController.swift
@@ -13,8 +13,6 @@ class ViewController: UIViewController{
   @IBOutlet weak var tableView: UITableView!
   var icons: [Icon]
   
-  
-  
   required init?(coder aDecoder: NSCoder) {
     icons = [Icon]()
     
@@ -43,40 +41,28 @@ class ViewController: UIViewController{
     let row12Icon = Icon(title: "Volume", subtitle: "Put it louder", imageName: "icons_12")
     icons.append(row12Icon)
     
-    
     super.init(coder: aDecoder)
   }
   
-  
-
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     navigationItem.rightBarButtonItem = editButtonItem()
-    
     automaticallyAdjustsScrollViewInsets = false
-    
   }
-
-
+  
 }
 
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
   
-  
-  
   override func setEditing(editing: Bool, animated: Bool) {
-    
     super.setEditing(editing, animated: animated)
     if editing {
       tableView.setEditing(true, animated: true)
     } else {
       tableView.setEditing(false, animated: true)
     }
-    
   }
-  
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return icons.count
@@ -84,43 +70,26 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("IconCell", forIndexPath: indexPath)
-  
-    let icon = icons[indexPath.row]
     
+    let icon = icons[indexPath.row]
     cell.textLabel?.text = icon.title
     cell.detailTextLabel?.text = icon.subtitle
-    
     if let imageView = cell.imageView, iconImage = icon.image {
       imageView.image = iconImage
     }
     
-  
-  
-  return cell
-}
-  
-  
-  
-  
+    return cell
+  }
   
   func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
     
     if editingStyle == .Delete {
       
-    icons.removeAtIndex(indexPath.row)
-     //tableView.reloadData()
+      icons.removeAtIndex(indexPath.row)
+      //tableView.reloadData()
       tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
     
-    
   }
   
-  
-  
-  
-  
-  
-  
-  
-
 }
