b73c5bf606aed81e1408cd7c483ab66f5a33c41c
Silvostok <testing repo>
2016-02-22 22:28:50 +0100

    color status

--- project 03/Search/Search/ViewController.swift
+++ project 03/Search/Search/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    automaticallyAdjustsScrollViewInsets = false
+    //automaticallyAdjustsScrollViewInsets = false
     tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     searchBar.becomeFirstResponder()
   }
