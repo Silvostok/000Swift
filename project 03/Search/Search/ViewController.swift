84c41a325fbf592fde6ff70dd55453abe66f2f1b
Silvostok <testing repo>
2016-02-22 13:03:45 +0100

    new files

--- project 03/Search/Search/ViewController.swift
+++ project 03/Search/Search/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var tableView: UITableView!
+  
+  var cities = ["Thebes", "Jerusalem", "Nineveh", "Tyre", "Babylon", "Memphis", "Athens", "Carthago", "Syracuse", "Alexandria", "Anuradhapura", "Rome", "Pataliputra", "Constantinople"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    automaticallyAdjustsScrollViewInsets = false
+    tableView.contentInset = UIEdgeInsets(top: 108, left: 0, bottom: 0, right: 0)
+    searchBar.becomeFirstResponder()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    
-  }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return cities.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    let city = cities[indexPath.row]
+    cell.textLabel?.text = city
+    return cell
+  }
+  
+}
+
+
+extension ViewController: UISearchBarDelegate {
+  func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+    searchBar.resignFirstResponder()
+    print("The search text is: '\(searchBar.text!)'")
+  }
+  
+  func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+    return .TopAttached
+  }
+  
+}
