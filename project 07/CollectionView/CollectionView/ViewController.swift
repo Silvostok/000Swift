bc847a0f658aa45323ef9fd008605c21a50a96ae
Silvostok <testing repo>
2016-02-28 21:32:45 +0100

    new

---
+++ project 07/CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Miguel Angel Rubio Caballero on 28/02/16.
+//  Copyright © 2016 MiguelRubio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+  
+
+
+}
+
